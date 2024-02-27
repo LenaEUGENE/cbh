59adbb9e562b23a7d169e560886a915136907ef0
Lena EUGÈNE <lenaeugene@gmail.com>
2024-02-27 23:19:44 +0100

    plenty but mostly the header

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -17,17 +17,17 @@ module.exports =  {
 		},
 		fontSize: {
 			/* body font-sizes */
-			"fs-tiny": ["clamp(0.79rem, 0.06vw + 0.78rem, 0.82rem)", "1.6"],
-			"fs-small": ["clamp(0.89rem, 0.17vw + 0.85rem, 0.99rem)", "1.5"],
-			"fs-base": ["clamp(1rem, 0.31vw + 0.93rem, 1.19rem)", "1.5"],
-			"fs-medium": ["clamp(1.27rem, 0.73vw + 1.09rem, 1.71rem)", "1.4"],
+			"fs-tiny": ["clamp(0.625rem, 0.191vw + 0.587rem, 0.75rem)", "1.6"],
+			"fs-small": ["clamp(0.75rem, 0.191vw + 0.712rem, 0.875rem)", "1.5"],
+			"fs-base": ["clamp(0.875rem, 0.382vw + 0.799rem, 1.125rem)", "1.5"],
+			"fs-medium": ["clamp(1.25rem, 0.382vw + 1.174rem, 1.5rem)", "1.4"],
 
 			/* heading font-sizes */
-			"fs-h-xlarge": ["clamp(2.44rem, 2.57vw + 1.84rem, 4rem)", "1.1"],
-			"fs-h-large": ["clamp(1.95rem, 1.73vw + 1.55rem, 3rem)", "1.2"],
-			"fs-h-medium": ["(1.56rem, 1.13vw + 1.3rem, 2.25rem)", "1.2"],
-			"fs-h-small": ["clamp(1.25rem, 0.72vw + 1.08rem, 1.69rem)", "1.2"],
-			"fs-h-tiny": ["clamp(1rem, 0.44vw + 0.9rem, 1.27rem)", "1.3"],
+			"fs-h-xlarge": ["clamp(2.5rem, 2.294vw + 2.041rem, 4rem)", "1.1"],
+			"fs-h-large": ["clamp(2rem, 0.765vw + 1.847rem, 2.5rem)", "1.2"],
+			"fs-h-medium": ["clamp(1.75rem, 0.382vw + 1.674rem, 2rem)", "1.2"],
+			"fs-h-small": ["clamp(1.125rem, 0.956vw + 0.934rem, 1.75rem)", "1.2"],
+			"fs-h-tiny": ["clamp(1.15rem, 1.125, 1.125rem)", "1.3"],
 		},
 		extend: {},
 	},
